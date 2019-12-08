3052633d5dac1a1c01bf4ad7a637fcc21fd203cc
jcom619 <46643961+jcom619@users.noreply.github.com>
2019-12-08 12:13:47 -0800

    multi detection update

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -41,11 +41,14 @@ class Signin extends React.Component {
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
+          <form>
+
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input
+                  auto-fill="email"
                   className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="email"
                   name="email-address"
@@ -56,6 +59,7 @@ class Signin extends React.Component {
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                 <input
+                  auto="current-password"
                   className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="password"
                   name="password"
@@ -75,6 +79,9 @@ class Signin extends React.Component {
             <div className="lh-copy mt3">
               <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             </div>
+
+          </form>
+
           </div>
         </main>
       </article>
